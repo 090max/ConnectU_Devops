d9c3a4518247e6c26de79831004d5da1d490b276
Kapil Israni <090max@gmail.com>
2020-12-01 17:56:31 +0530

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const cors = require("cors");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const router = require("./router");
 
